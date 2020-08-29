cbaa7d065f9c81bd133a16a029cc6ae442f66726
Lee bosung <dlqh406@gmail.com>
2020-08-29 12:43:10 +0900

    오류발견 : Command CompileSwiftSources failed with a nonzero exit code
    스토리보드 상 interfaceController에 있는 엘리먼트? 들을 삭제했는데
    Command CompileSwiftSources failed with a nonzero exit code
    라는 오류코드가 뜹니다

--- App/watch-out-app/watch-out-app WatchKit Extension/InterfaceController.swift
+++ App/watch-out-app/watch-out-app WatchKit Extension/InterfaceController.swift
@@ -11,51 +11,70 @@ import Foundation
 import WatchConnectivity
 import SwiftUI
 
-class InterfaceController: WKInterfaceController {
+import SwiftUI
 
-    @IBOutlet weak var alertTitle: WKInterfaceLabel!
-    @IBOutlet weak var content: WKInterfaceLabel!
-    @IBOutlet weak var img: WKInterfaceImage!
-    
-    let session = WCSession.default
-    
-    override func awake(withContext context: Any?) {
-        super.awake(withContext: context)
-        
-        self.img.setImage(UIImage(named: "fire"))
-        
-        // Configure interface objects here.
-        session.delegate = self
-        session.activate()
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-    
-
 }
 
-extension InterfaceController: WCSessionDelegate {
-  
-  func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-  }
-  
-  func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
-    print("received data: \(message)")
-    if let t = message["title"] as? String {
-        self.alertTitle.setText(t)
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
     }
-    if let c = message["content"] as? String {
-        self.content.setText(c)
-    }
-
-
-  }
 }
+
+
+
+
+
+//
+//class InterfaceController: WKInterfaceController {
+//
+//    @IBOutlet weak var alertTitle: WKInterfaceLabel!
+//    @IBOutlet weak var content: WKInterfaceLabel!
+//    @IBOutlet weak var img: WKInterfaceImage!
+//
+//    let session = WCSession.default
+//
+//    override func awake(withContext context: Any?) {
+//        super.awake(withContext: context)
+//
+//        self.img.setImage(UIImage(named: "fire"))
+//
+//        // Configure interface objects here.
+//        session.delegate = self
+//        session.activate()
+//    }
+//
+//    override func willActivate() {
+//        // This method is called when watch view controller is about to be visible to user
+//        super.willActivate()
+//    }
+//
+//    override func didDeactivate() {
+//        // This method is called when watch view controller is no longer visible
+//        super.didDeactivate()
+//    }
+//
+//
+//}
+//
+//extension InterfaceController: WCSessionDelegate {
+//
+//  func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+//  }
+//
+//  func session(_ session: WCSession, didReceiveMessage message: [String : Any]) {
+//    print("received data: \(message)")
+//    if let t = message["title"] as? String {
+//        self.alertTitle.setText(t)
+//    }
+//    if let c = message["content"] as? String {
+//        self.content.setText(c)
+//    }
+//
+//
+//  }
+//}
