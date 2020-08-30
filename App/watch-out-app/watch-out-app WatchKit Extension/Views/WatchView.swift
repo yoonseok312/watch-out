67f944c1d7f6db94ff275cfbf7c9649bad841668
Lee bosung <dlqh406@gmail.com>
2020-08-30 23:30:11 +0900

    음성인식 단어에 따른 아이콘변경(오류해결), 박스안 텍스트 정렬

--- App/watch-out-app/watch-out-app WatchKit Extension/Views/WatchView.swift
+++ App/watch-out-app/watch-out-app WatchKit Extension/Views/WatchView.swift
@@ -12,33 +12,36 @@ import Foundation
 import SwiftUI
 import WatchConnectivity
 
-var title: String = "default"
+
 
 struct WatchView: View {
     @EnvironmentObject var viewModel: WatchEnvironment
     static let gradientStart = Color(red: 255.0 / 255, green: 60.0 / 255, blue: 0.0 / 255)
     static let gradientEnd =  Color(red: 255 / 255, green: 108 / 255, blue: 63 / 255)
 
-    var detectedWord : String = viewModel.word
+//    var detectedWord : String = viewModel.word
       var finalIcon : String = "map"
 
-    switch detectedWord {
-    case "car":
-        finalIcon = "car"
-    case "yes":
-        finalIcon = "hare"
-    case "right":
-        finalIcon = "pin"
-    default:
-        finalIcon = "tortoise"
-    }
+ 
   
 var body: some View {
    
         VStack{
             Spacer()
             HStack{
-                Image(systemName: finalIcon).foregroundColor(.white).font(.system(size: 23)).padding(.horizontal,2)
+                // 아이콘 변경 코드
+                if(viewModel.word == "yes"){
+                     Image(systemName: "car").foregroundColor(.white).font(.system(size: 23)).padding(.horizontal,2)
+                }else if (viewModel.word == "no"){
+                     Image(systemName: "map").foregroundColor(.white).font(.system(size: 23)).padding(.horizontal,2)
+                }else if (viewModel.word == "right"){
+                     Image(systemName: "snow").foregroundColor(.white).font(.system(size: 23)).padding(.horizontal,2)
+                }else if (viewModel.word == "left"){
+                     Image(systemName: "flanme.fill").foregroundColor(.white).font(.system(size: 23)).padding(.horizontal,2)
+                    }
+                else{
+                     Image(systemName: "heart.fill").foregroundColor(.white).font(.system(size: 23)).padding(.horizontal,2)
+                }
                 Text("위험 신호감지!").font(.system(size: 23, weight: .bold))
             }
             
@@ -54,13 +57,11 @@ var body: some View {
                                ))
                                .frame(width: 185, height: 120)
                     
-//                    VStack(alignment: .leading) {
-//                        Text("근처에서 소ㅊ").font(.system(size: 18, weight: .black)).padding(.vertical,7).foregroundColor(Color.init(red: 0, green: 0, blue: 0))
-                        
-                    Text(viewModel.word).font(.system(size: 37, weight: .black)).foregroundColor(Color.init(red: 255.0, green: 255.0, blue: 255.0))
-//
-//                        HStack(alignment:.lastTextBaseline){
-//                            Text("가 들렸습니다").font(.system(size: 18, weight: .black)).padding(.vertical,7).padding(.leading,60).foregroundColor(Color.init(red: 0, green: 0, blue: 0))
+                    VStack(alignment: .center) {
+                        Text("근처에서 소리").font(.system(size: 18, weight: .black)).padding(.vertical,7).padding(.trailing,55).foregroundColor(Color.init(red: 0, green: 0, blue: 0))
+                        Text(viewModel.word+"소리").font(.system(size: 37, weight: .black)).foregroundColor(Color.init(red: 255.0, green: 255.0, blue: 255.0))
+                        HStack(alignment:.lastTextBaseline){
+                            Text("가 들렸습니다").font(.system(size: 18, weight: .black)).padding(.vertical,7).padding(.leading,60).foregroundColor(Color.init(red: 0, green: 0, blue: 0))
                         }
                     }
                    
@@ -76,4 +77,6 @@ var body: some View {
 //        Alert_A()
 //    }
 //}
+
+    }}
 
