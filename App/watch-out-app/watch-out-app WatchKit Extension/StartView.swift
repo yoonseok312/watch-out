01c80f997026d101542e02c4c7139228fc2534ec
Doyeon Kim <49015100+yammayamm@users.noreply.github.com>
2020-08-30 14:35:16 +0900

    logo.png & StartView 추가
    View만 추가했지 실제로 아직 작동은 안함.

---
+++ App/watch-out-app/watch-out-app WatchKit Extension/StartView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct StartView: View {
+  
+  @State var nextScreenShown = false
+  
+  var body: some View {
+
+    VStack{
+        Image("logo")
+        .resizable()
+        .frame(width: 112.0, height: 112.0)
+        Spacer()
+        NavigationLink(destination: ContentView()) {
+            Text("위험신호 감지 시작 ").bold()
+        }
+    }
+  }
+}
+
+struct StartView_Previews: PreviewProvider {
+  static var previews: some View {
+    StartView()
+  }
+}
+
+extension Color {
+  init(_ hex: UInt32, opacity:Double = 1.0) {
+    let red = Double((hex & 0xff0000) >> 16) / 255.0
+    let green = Double((hex & 0xff00) >> 8) / 255.0
+    let blue = Double((hex & 0xff) >> 0) / 255.0
+    self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+  }
+}
+
