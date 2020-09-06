590f7cbee05540523bb7cb08ebf97f5e92e315d4
dlqh406 <55032984+dlqh406@users.noreply.github.com>
2020-09-06 23:19:27 +0900

    Update ContainerView.swift

--- App/watch-out-app/watch-out-app WatchKit Extension/Views/ContainerView.swift
+++ App/watch-out-app/watch-out-app WatchKit Extension/Views/ContainerView.swift
@@ -24,7 +24,47 @@ struct ContainerView: View {
 }
 
 struct DefaultView: View {
+    @State private var animateStrokeStart = true
+    @State private var animateStrokeEnd = true
+    @State private var isRotating = true
+
   var body: some View {
-    Text("아이폰에서 소리탐지중...")
+    VStack {
+          Spacer()
+             Spacer()
+             Spacer()
+           ZStack {
+               Image(systemName: "antenna.radiowaves.left.and.right").foregroundColor(.white).font(.system(size: 50))
+             Circle()
+               .trim(from: animateStrokeStart ? 1/3 : 1/9, to: animateStrokeEnd ? 2/5 : 1)
+               .stroke(lineWidth: 10)
+               .frame(width: 100, height: 100)
+               .foregroundColor(Color(red: 255, green: 0, blue:1))
+               .rotationEffect(.degrees(isRotating ? 360 : 0))
+               .onAppear() {
+                 
+                 withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) {
+                   self.isRotating.toggle()
+                 }
+                 
+                 withAnimation(Animation.linear(duration: 1).delay(0.5).repeatForever(autoreverses: true)) {
+                   self.animateStrokeStart.toggle()
+                 }
+                 
+                 withAnimation(Animation.linear(duration: 1).delay(0.5).repeatForever(autoreverses: true)) {
+                   self.animateStrokeEnd.toggle()
+                 }
+             }
+                  
+           }
+         Spacer()
+               Spacer()
+           Spacer()
+            
+                 Text("위험 소리 탐지중...")
+                       .fontWeight(.bold)
+            
+         }
+       }
+
   }
-}
